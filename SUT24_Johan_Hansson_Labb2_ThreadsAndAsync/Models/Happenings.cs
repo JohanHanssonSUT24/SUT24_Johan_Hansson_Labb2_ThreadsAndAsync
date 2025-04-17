d766b8b8b3754b9ec47b286e8a769744caaffd44
johan <johan@LAPTOP-6KO4S5LR>
2025-04-17 14:50:59 +0200

    Added new function to StartRace

--- SUT24_Johan_Hansson_Labb2_ThreadsAndAsync/Models/Happenings.cs
+++ SUT24_Johan_Hansson_Labb2_ThreadsAndAsync/Models/Happenings.cs
@@ -9,31 +9,37 @@ namespace SUT24_Johan_Hansson_Labb2_ThreadsAndAsync.Models
     public class Happenings
     {
         private static readonly Random random = new Random();
-        public async Task<int> RaceHappening(Car car)
+        public async Task<string> RaceHappening(Car car)
         {
             int happening = random.Next(1, 51);
+            string description = "";
 
             if (happening == 1)
             {
-                Console.WriteLine($"Nu fick {car.Name} slut på bensin. Depåstoppet kommer kosta 15 sekunder");
+                description = "slut på bensin. Depåstoppet kommer kosta 15 sekunder";
+                //Console.WriteLine($"Nu fick {car.Name} {description}");
                 await Task.Delay(15000);
             }
             else if (happening <= 3)
             {
-                Console.WriteLine($"Ett däck exploderar för {car.Name}. Men teamet är snabba och löser detta på 10 sekunder.");
-                await Task.Delay(15000);
+                description = "Ett däck exploderade! Men teamet är snabbt och löser detta på 10 sekunder.";
+                //Console.WriteLine($"{car.Name} har lite problem. {description}");
+                await Task.Delay(10000);
             }
             else if (happening <= 8)
             {
-                Console.WriteLine($"En fågel har flugit in i {car.Name}. Det kommer ta 5 sekunder att få rutan ren.");
+                description = "En fågel har flugit in i rutan! Hoppas den inte skadade sig men det blir 5 dyra sekunders tillägg.";
+                //Console.WriteLine($"Vad händer med {car.Name}? {description}");
                 await Task.Delay(5000);
             }
             else if (happening <= 18)
             {
                 car.Speed = Math.Max(car.Speed - 1, 1);
-                Console.WriteLine($"{car.Name} har problem där ute. Motorn krånglar så hastigheten har gått ner till {car.Speed} km/h.");
+                description = $"Motorn krånglar så hastigheten har gått ner till {car.Speed} km/h.";
+                //Console.WriteLine($"{car.Name} har problem där ute. {description}");
             }
-            return happening;
+           
+                return description;
         }
 
     }
